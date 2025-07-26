e7e5d90c0ac5df02c45e29c776853364f8117c3a
Jennifer Taylor <dragonminded@dragonminded.com>
2025-07-26 23:58:46 +0000

    Begin a setup installer for productionizing.

---
+++ backend/setup.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+from setuptools import Command, setup
+
+
+setup(
+    name='critterchat',
+    version='0.0.1',
+    description='CritterChat chat room software.',
+    author='DragonMinded',
+    license='Public Domain',
+    packages=[
+        'critterchat',
+        'critterchat.common',
+        'critterchat.config',
+        'critterchat.data',
+        'critterchat.data.migrations',
+        'critterchat.data.migrations.versions',
+        'critterchat.http',
+        'critterchat.http.static',
+        'critterchat.http.templates',
+        'critterchat.service',
+    ],
+    install_requires=[
+        req for req in open('requirements.txt').read().split('\n') if len(req) > 0
+    ],
+    include_package_data=True,
+)
