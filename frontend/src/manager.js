ea6671a47a5c501d662f2b359c22c42f1810b4e3
Jennifer Taylor <dragonminded@dragonminded.com>
2025-08-12 19:23:34 +0000

    Remove unnecessary document onready since this is done at the top level.

--- frontend/src/manager.js
+++ frontend/src/manager.js
@@ -16,163 +16,161 @@ export function manager(socket) {
     var inputState = new InputState();
     var screenState = new ScreenState();
 
-    $( document ).ready(() => {
-        var settings = {};
-        var size = $( window ).width() <= 700 ? "mobile" : "desktop";
+    var settings = {};
+    var size = $( window ).width() <= 700 ? "mobile" : "desktop";
 
-        var menuInst = new Menu(eventBus, screenState, inputState, size);
-        var messagesInst = new Messages(eventBus, screenState, inputState, size);
-        var infoInst = new Info(eventBus, screenState, inputState, size);
-        var searchInst = new Search(eventBus, screenState, inputState);
+    var menuInst = new Menu(eventBus, screenState, inputState, size);
+    var messagesInst = new Messages(eventBus, screenState, inputState, size);
+    var infoInst = new Info(eventBus, screenState, inputState, size);
+    var searchInst = new Search(eventBus, screenState, inputState);
 
-        socket.on('connect', () => {
-            // Ask for our list of rooms that we're in.
-            socket.emit('roomlist', {});
+    socket.on('connect', () => {
+        // Ask for our list of rooms that we're in.
+        socket.emit('roomlist', {});
 
-            // Ask for our profile and last settings so we can refresh where we left off.
-            socket.emit('profile', {});
-            socket.emit('lastsettings', {});
+        // Ask for our profile and last settings so we can refresh where we left off.
+        socket.emit('profile', {});
+        socket.emit('lastsettings', {});
 
-            // Ask for any server MOTD or admin messages.
-            socket.emit('motd', {});
+        // Ask for any server MOTD or admin messages.
+        socket.emit('motd', {});
+    });
+
+    socket.on('reload', () => {
+        // Server wants us to reload, probably to de-auth ourselves after a remote logout.
+        window.location.reload();
+    });
+
+    $( window ).on( "resize", () => {
+        const width = $( window ).width();
+        const newSize = width <= 700 ? "mobile" : "desktop";
+
+        if (newSize != size) {
+            size = newSize;
+            eventBus.emit('resize', size);
+        }
+    });
+
+    socket.on('welcome', (msg) => {
+        var html = "";
+        msg.rooms.forEach((result) => {
+            var id = result.roomid;
+            var type = result['public'] ? 'room' : 'avatar';
+
+            html += '<div class="item" id="' + id + '">';
+            html += '  <div class="icon ' + type + '">';
+            html += '    <img src="' + result.icon + '" />';
+            html += '  </div>';
+            html += '  <div class="name-wrapper"><div class="name">' + escapeHtml(result.name) + '</div></div>';
+            html += '</div>';
         });
 
-        socket.on('reload', () => {
-            // Server wants us to reload, probably to de-auth ourselves after a remote logout.
-            window.location.reload();
-        });
-
-        $( window ).on( "resize", () => {
-            const width = $( window ).width();
-            const newSize = width <= 700 ? "mobile" : "desktop";
-
-            if (newSize != size) {
-                size = newSize;
-                eventBus.emit('resize', size);
+        displayInfo(
+            '<div class="welcome-message">' + msg.message + '</div><div class="results">' + html + '</div>',
+            'okay!',
+            () => {
+                socket.emit('welcomeaccept', {});
             }
-        });
+        );
+    });
 
-        socket.on('welcome', (msg) => {
-            var html = "";
-            msg.rooms.forEach((result) => {
-                var id = result.roomid;
-                var type = result['public'] ? 'room' : 'avatar';
+    socket.on('error', (msg) => {
+        displayInfo(
+            msg.error,
+            'okay!',
+        );
+    })
 
-                html += '<div class="item" id="' + id + '">';
-                html += '  <div class="icon ' + type + '">';
-                html += '    <img src="' + result.icon + '" />';
-                html += '  </div>';
-                html += '  <div class="name-wrapper"><div class="name">' + escapeHtml(result.name) + '</div></div>';
-                html += '</div>';
-            });
+    socket.on('roomlist', (msg) => {
+        menuInst.setRooms(msg.rooms);
+        if (msg.counts) {
+            menuInst.setBadges(msg.counts);
+        }
+        infoInst.setRooms(msg.rooms);
+        if (msg.selected) {
+            menuInst.selectRoom(msg.selected);
+        }
+        messagesInst.setRooms(msg.rooms);
+    });
 
-            displayInfo(
-                '<div class="welcome-message">' + msg.message + '</div><div class="results">' + html + '</div>',
-                'okay!',
-                () => {
-                    socket.emit('welcomeaccept', {});
-                }
-            );
-        });
+    socket.on('lastsettings', (msg) => {
+        settings = msg;
+        menuInst.setLastSettings(msg);
+        messagesInst.setLastSettings(msg);
+        infoInst.setLastSettings(msg);
+    });
 
-        socket.on('error', (msg) => {
-            displayInfo(
-                msg.error,
-                'okay!',
-            );
-        })
+    socket.on('profile', (msg) => {
+        // This should never change, but if we have info about it, let's update anyway.
+        window.username = msg.username;
+        menuInst.setProfile(msg);
+    });
 
-        socket.on('roomlist', (msg) => {
-            menuInst.setRooms(msg.rooms);
-            if (msg.counts) {
-                menuInst.setBadges(msg.counts);
-            }
-            infoInst.setRooms(msg.rooms);
-            if (msg.selected) {
-                menuInst.selectRoom(msg.selected);
-            }
-            messagesInst.setRooms(msg.rooms);
-        });
+    socket.on('chathistory', (msg) => {
+        messagesInst.setOccupants(msg.roomid, msg.occupants);
+        infoInst.setOccupants(msg.roomid, msg.occupants);
+        messagesInst.updateHistory(msg.roomid, msg.history, msg.lastseen);
+    });
 
-        socket.on('lastsettings', (msg) => {
-            settings = msg;
-            menuInst.setLastSettings(msg);
-            messagesInst.setLastSettings(msg);
-            infoInst.setLastSettings(msg);
-        });
+    socket.on('chatactions', (msg) => {
+        messagesInst.updateActions(msg.roomid, msg.actions);
+        menuInst.updateActions(msg.roomid, msg.actions);
+        infoInst.updateActions(msg.roomid, msg.actions);
+    });
 
-        socket.on('profile', (msg) => {
-            // This should never change, but if we have info about it, let's update anyway.
-            window.username = msg.username;
-            menuInst.setProfile(msg);
-        });
+    socket.on('searchrooms', (msg) => {
+        searchInst.populateResults(msg.rooms);
+    });
 
-        socket.on('chathistory', (msg) => {
-            messagesInst.setOccupants(msg.roomid, msg.occupants);
-            infoInst.setOccupants(msg.roomid, msg.occupants);
-            messagesInst.updateHistory(msg.roomid, msg.history, msg.lastseen);
-        });
+    socket.on('emotechanges', (msg) => {
+        messagesInst.addEmotes(msg.additions);
+        messagesInst.deleteEmotes(msg.deletions);
+    });
 
-        socket.on('chatactions', (msg) => {
-            messagesInst.updateActions(msg.roomid, msg.actions);
-            menuInst.updateActions(msg.roomid, msg.actions);
-            infoInst.updateActions(msg.roomid, msg.actions);
-        });
+    eventBus.on('selectroom', (roomid) => {
+        settings.roomid = roomid;
 
-        socket.on('searchrooms', (msg) => {
-            searchInst.populateResults(msg.rooms);
-        });
+        messagesInst.setRoom(roomid);
+        infoInst.setRoom(roomid);
+        socket.emit('updatesettings', settings);
+        socket.emit('chathistory', {roomid: roomid});
+    });
 
-        socket.on('emotechanges', (msg) => {
-            messagesInst.addEmotes(msg.additions);
-            messagesInst.deleteEmotes(msg.deletions);
-        });
+    eventBus.on('updateinfo', (info) => {
+        settings.info = info;
 
-        eventBus.on('selectroom', (roomid) => {
-            settings.roomid = roomid;
+        socket.emit('updatesettings', settings);
+    });
 
-            messagesInst.setRoom(roomid);
-            infoInst.setRoom(roomid);
-            socket.emit('updatesettings', settings);
-            socket.emit('chathistory', {roomid: roomid});
-        });
+    eventBus.on('updateprofile', (profile) => {
+        socket.emit('updateprofile', profile);
+    });
 
-        eventBus.on('updateinfo', (info) => {
-            settings.info = info;
+    eventBus.on('leaveroom', (roomid) => {
+        socket.emit('leaveroom', {'roomid': roomid})
 
-            socket.emit('updatesettings', settings);
-        });
+        messagesInst.closeRoom(roomid);
+        infoInst.closeRoom(roomid);
+        menuInst.closeRoom(roomid);
+    });
 
-        eventBus.on('updateprofile', (profile) => {
-            socket.emit('updateprofile', profile);
-        });
+    eventBus.on('joinroom', (roomoruserid) => {
+        socket.emit('joinroom', {'roomid': roomoruserid})
+    });
 
-        eventBus.on('leaveroom', (roomid) => {
-            socket.emit('leaveroom', {'roomid': roomid})
+    eventBus.on('updateroom', (msg) => {
+        socket.emit('updateroom', {'roomid': msg.roomid, 'details': msg.details});
+    });
 
-            messagesInst.closeRoom(roomid);
-            infoInst.closeRoom(roomid);
-            menuInst.closeRoom(roomid);
-        });
+    eventBus.on('message', (msg) => {
+        socket.emit('message', msg);
+    });
 
-        eventBus.on('joinroom', (roomoruserid) => {
-            socket.emit('joinroom', {'roomid': roomoruserid})
-        });
+    eventBus.on('searchrooms', (value) => {
+        socket.emit('searchrooms', {'name': value})
+    });
 
-        eventBus.on('updateroom', (msg) => {
-            socket.emit('updateroom', {'roomid': msg.roomid, 'details': msg.details});
-        });
-
-        eventBus.on('message', (msg) => {
-            socket.emit('message', msg);
-        });
-
-        eventBus.on('searchrooms', (value) => {
-            socket.emit('searchrooms', {'name': value})
-        });
-
-        eventBus.on('lastaction', (value) => {
-            socket.emit('lastaction', value);
-        });
+    eventBus.on('lastaction', (value) => {
+        socket.emit('lastaction', value);
     });
 }
