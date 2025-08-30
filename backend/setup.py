4d2d46e996577cf2221d71e71074e82d6d98f0fd
Jennifer Taylor <dragonminded@dragonminded.com>
2025-08-30 14:51:22 +0000

    Fix pip deprecation warning when installing.

--- backend/setup.py
+++ backend/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='critterchat',
-    version='0.0.10',
+    version='0.0.11',
     description='CritterChat chat room software.',
     author='DragonMinded',
     license='Public Domain',
