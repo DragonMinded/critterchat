4938b0f9faeff99caba52bc276abae66ba4439a9
Jennifer Taylor <dragonminded@dragonminded.com>
2025-09-06 16:21:37 +0000

    Bump setup revision.

--- backend/setup.py
+++ backend/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='critterchat',
-    version='0.0.16',
+    version='0.0.17',
     description='CritterChat chat room software.',
     author='DragonMinded',
     license='Public Domain',
