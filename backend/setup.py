114d1ebebe4a60e89788e620b64d2c97ec7a5d1c
Jennifer Taylor <dragonminded@dragonminded.com>
2025-09-16 16:33:05 +0000

    Explicit background color for all profile images.

--- backend/setup.py
+++ backend/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='critterchat',
-    version='0.0.23',
+    version='0.0.24',
     description='CritterChat chat room software.',
     author='DragonMinded',
     license='Public Domain',
