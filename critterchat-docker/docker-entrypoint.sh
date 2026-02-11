fa376bfca088b48b0e15b8f75f423c7f413d78d6
ThawnyRose <thawnyrose@gmail.com>
2026-02-11 16:44:16 -0700

    Add support for setting the '--nginx-proxy ' option

--- critterchat-docker/docker-entrypoint.sh
+++ critterchat-docker/docker-entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
+
+OPTS=""
+
+if [ -n "$BEHIND_NGINX_PROXY" ]; then
+	OPTS="$OPTS --nginx-proxy 1"
+fi
+
 if python3 -m critterchat.manage --config config.yaml database create --existing-okay; then
 	python3 -m critterchat.manage --config config.yaml database upgrade
-	python3 -m critterchat --config config.yaml
+	echo "Running Command: python3 -m critterchat --config config.yaml $OPTS"
+	python3 -m critterchat --config config.yaml $OPTS
 else
 	exit 1;
 fi
