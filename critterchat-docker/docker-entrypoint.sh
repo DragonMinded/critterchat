ee1116942121d37650359c5e045bb90d3da4552e
ThawnyRose <thawnyrose@gmail.com>
2026-02-10 23:00:48 -0700

    Handle Database Upgrade

--- critterchat-docker/docker-entrypoint.sh
+++ critterchat-docker/docker-entrypoint.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 if python3 -m critterchat.manage --config config.yaml database create --existing-okay; then
+	python3 -m critterchat.manage --config config.yaml database upgrade
 	python3 -m critterchat --config config.yaml
 else
 	exit 1;
