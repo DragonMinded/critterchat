2a7ccd008a0f047cc469862e35328d0bdbb210f1
Jennifer Taylor <dragonminded@dragonminded.com>
2026-06-03 00:17:16 +0000

    Add nested transaction tests.

--- backend/tests/data/test_base.py
+++ backend/tests/data/test_base.py
@@ -120,10 +120,8 @@ class TestBase:
             with second.transaction():
                 second.execute("INSERT INTO test_rollback (column1, column2) VALUES ('test1', 'test2')")
                 raise Exception("Should cause rollback!")
-
         except Exception:
             pass
-
         second.execute("INSERT INTO test_rollback (column1, column2) VALUES ('always3', 'always4')")
         secondconn.close()
 
@@ -140,4 +138,137 @@ class TestBase:
         finalconn = db.connect()
         final = BaseData(config, cast(ConnectionLike, finalconn))
         final.execute("DROP TABLE IF EXISTS test_rollback")
+        finalconn.close()
+
+    def test_commit_txn_nested_commit(self, db: Engine) -> None:
+        """
+        Tests that we commit inside transactions as expected.
+        """
+
+        config = MockConfig()
+
+        # Create a test table to operate on.
+        firstconn = db.connect()
+        first = BaseData(config, cast(ConnectionLike, firstconn))
+        first.execute("CREATE TABLE IF NOT EXISTS test_nested_commit (column1 VARCHAR(32), column2 VARCHAR(32))")
+        firstconn.close()
+
+        # Now, operate on the table in the second connection.
+        secondconn = db.connect()
+        second = BaseData(config, cast(ConnectionLike, secondconn))
+        second.execute("INSERT INTO test_nested_commit (column1, column2) VALUES ('always1', 'always2')")
+        with second.transaction():
+            second.execute("INSERT INTO test_nested_commit (column1, column2) VALUES ('test1', 'test2')")
+            with second.transaction():
+                second.execute("INSERT INTO test_nested_commit (column1, column2) VALUES ('test3', 'test4')")
+            second.execute("INSERT INTO test_nested_commit (column1, column2) VALUES ('test5', 'test6')")
+        second.execute("INSERT INTO test_nested_commit (column1, column2) VALUES ('always3', 'always4')")
+        secondconn.close()
+
+        # Now, create another connection and ensure those rows exist.
+        thirdconn = db.connect()
+        third = BaseData(config, cast(ConnectionLike, thirdconn))
+        entries = list(third.execute("SELECT * FROM test_nested_commit").mappings())
+        assert len(entries) == 5
+        assert entries[0] == {"column1": "always1", "column2": "always2"}
+        assert entries[1] == {"column1": "test1", "column2": "test2"}
+        assert entries[2] == {"column1": "test3", "column2": "test4"}
+        assert entries[3] == {"column1": "test5", "column2": "test6"}
+        assert entries[4] == {"column1": "always3", "column2": "always4"}
+        thirdconn.close()
+
+        # Finally, clean up.
+        finalconn = db.connect()
+        final = BaseData(config, cast(ConnectionLike, finalconn))
+        final.execute("DROP TABLE IF EXISTS test_nested_commit")
+        finalconn.close()
+
+    def test_commit_txn_nested_inner_rollback(self, db: Engine) -> None:
+        """
+        Tests that we roll back inside transactions as expected.
+        """
+
+        config = MockConfig()
+
+        # Create a test table to operate on.
+        firstconn = db.connect()
+        first = BaseData(config, cast(ConnectionLike, firstconn))
+        first.execute("CREATE TABLE IF NOT EXISTS test_nested_inner_rollback (column1 VARCHAR(32), column2 VARCHAR(32))")
+        firstconn.close()
+
+        # Now, operate on the table in the second connection.
+        secondconn = db.connect()
+        second = BaseData(config, cast(ConnectionLike, secondconn))
+        second.execute("INSERT INTO test_nested_inner_rollback (column1, column2) VALUES ('always1', 'always2')")
+        with second.transaction():
+            second.execute("INSERT INTO test_nested_inner_rollback (column1, column2) VALUES ('test1', 'test2')")
+            try:
+                with second.transaction():
+                    second.execute("INSERT INTO test_nested_inner_rollback (column1, column2) VALUES ('test3', 'test4')")
+                    raise Exception("Should cause rollback!")
+            except Exception:
+                pass
+            second.execute("INSERT INTO test_nested_inner_rollback (column1, column2) VALUES ('test5', 'test6')")
+        second.execute("INSERT INTO test_nested_inner_rollback (column1, column2) VALUES ('always3', 'always4')")
+        secondconn.close()
+
+        # Now, create another connection and ensure those rows exist.
+        thirdconn = db.connect()
+        third = BaseData(config, cast(ConnectionLike, thirdconn))
+        entries = list(third.execute("SELECT * FROM test_nested_inner_rollback").mappings())
+        assert len(entries) == 4
+        assert entries[0] == {"column1": "always1", "column2": "always2"}
+        assert entries[1] == {"column1": "test1", "column2": "test2"}
+        assert entries[2] == {"column1": "test5", "column2": "test6"}
+        assert entries[3] == {"column1": "always3", "column2": "always4"}
+        thirdconn.close()
+
+        # Finally, clean up.
+        finalconn = db.connect()
+        final = BaseData(config, cast(ConnectionLike, finalconn))
+        final.execute("DROP TABLE IF EXISTS test_nested_inner_rollback")
+        finalconn.close()
+
+    def test_commit_txn_nested_outer_rollback(self, db: Engine) -> None:
+        """
+        Tests that we roll back inside transactions as expected.
+        """
+
+        config = MockConfig()
+
+        # Create a test table to operate on.
+        firstconn = db.connect()
+        first = BaseData(config, cast(ConnectionLike, firstconn))
+        first.execute("CREATE TABLE IF NOT EXISTS test_nested_outer_rollback (column1 VARCHAR(32), column2 VARCHAR(32))")
+        firstconn.close()
+
+        # Now, operate on the table in the second connection.
+        secondconn = db.connect()
+        second = BaseData(config, cast(ConnectionLike, secondconn))
+        second.execute("INSERT INTO test_nested_outer_rollback (column1, column2) VALUES ('always1', 'always2')")
+        try:
+            with second.transaction():
+                second.execute("INSERT INTO test_nested_outer_rollback (column1, column2) VALUES ('test1', 'test2')")
+                with second.transaction():
+                    second.execute("INSERT INTO test_nested_outer_rollback (column1, column2) VALUES ('test3', 'test4')")
+                second.execute("INSERT INTO test_nested_outer_rollback (column1, column2) VALUES ('test5', 'test6')")
+                raise Exception("Should cause rollback!")
+        except Exception:
+            pass
+        second.execute("INSERT INTO test_nested_outer_rollback (column1, column2) VALUES ('always3', 'always4')")
+        secondconn.close()
+
+        # Now, create another connection and ensure those rows exist.
+        thirdconn = db.connect()
+        third = BaseData(config, cast(ConnectionLike, thirdconn))
+        entries = list(third.execute("SELECT * FROM test_nested_outer_rollback").mappings())
+        assert len(entries) == 2
+        assert entries[0] == {"column1": "always1", "column2": "always2"}
+        assert entries[1] == {"column1": "always3", "column2": "always4"}
+        thirdconn.close()
+
+        # Finally, clean up.
+        finalconn = db.connect()
+        final = BaseData(config, cast(ConnectionLike, finalconn))
+        final.execute("DROP TABLE IF EXISTS test_nested_outer_rollback")
         finalconn.close()
